0e4d4ddfbfa58a20c22676c661ece1f1907ae68f
opengeekv2 <Fr33s1ng1ng33k>
2021-11-23 14:52:53 +0100

    Implement constructor injection of ApiService in App

--- src/Application/App.py
+++ src/Application/App.py
@@ -2,8 +2,8 @@ from src.Services.ApiService import ApiService
 
 
 class App:
-    def __init__(self):
-        self._api_service = ApiService()
+    def __init__(self, api_service: ApiService = ApiService()):
+        self._api_service = api_service
 
     def api_service(self) -> ApiService:
         return self._api_service
