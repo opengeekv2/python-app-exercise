6fbf3b9676f0d3106e1f59718fdfef510befa4be
opengeekv2 <Fr33s1ng1ng33k>
2021-11-23 15:01:53 +0100

    Remove unneded default parameter after changed main.py

--- src/Application/App.py
+++ src/Application/App.py
@@ -2,7 +2,7 @@ from src.Services.ApiService import ApiService
 
 
 class App:
-    def __init__(self, api_service: ApiService = ApiService()):
+    def __init__(self, api_service: ApiService):
         self._api_service = api_service
 
     def api_service(self) -> ApiService:
